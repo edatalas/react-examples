26fdc0027f9f07d0eeee2beb669c952744ab6896
Faru Nuri Sönmez <info@farunurisonmez.com>
2022-11-09 02:24:22 +0300

    Popup Component v0.0.18

--- src/components/popup/index.tsx
+++ src/components/popup/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Area } from './index.styled'
 
 const Popup = () => {
-  return <Area>v0.0.17 test</Area>
+  return <Area>v0.0.18 test</Area>
 }
 
 export default Popup
