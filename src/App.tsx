28e92bd230ab3440ef1b17732846bc381b74e768
Faru Nuri Sönmez <info@farunurisonmez.com>
2022-11-11 15:15:36 +0300

    gh-app v0.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React, {useState} from 'react';
 
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {Route, HashRouter, Router} from "react-router-dom";
+
 
 import * as ROUTES from "../src/contants/routes";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
     const [setIsAuth] = useState(false);
   return (
-          <Router>
-              <Route exact path={ROUTES.HOME}>
-                  <Home/>
-              </Route>
-              <Route path={ROUTES.LOGIN}>
-                  <Login setIsAuth={setIsAuth}/>
-              </Route>
-                  <Route component={NotFoundPage} />
-          </Router>
+      <HashRouter>
+          <Route exact path={ROUTES.HOME}>
+              <Home />
+          </Route>
+          <Route path={ROUTES.LOGIN}>
+              <Login setIsAuth={setIsAuth}/>
+          </Route>
+      </HashRouter>
   );
 }
 
