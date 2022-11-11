1d57c79e3a4554e224a25cb7b3bf2397e7300250
Faru Nuri Sönmez <info@farunurisonmez.com>
2022-11-11 04:46:21 +0300

    gh-app v0.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {Suspense, useState} from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter, BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import * as ROUTES from "../src/contants/routes";
 
