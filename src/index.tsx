17f3607c4ac0ccf5dff4efc1640ceafe26f25033
Faru Nuri Sönmez <info@farunurisonmez.com>
2022-11-11 05:48:15 +0300

    gh-app v0.0.1

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <HashRouter basename="/react-examples/">
+      <BrowserRouter basename="react-examples/">
           <App />
-      </HashRouter>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
