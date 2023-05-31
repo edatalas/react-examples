c9028339a4d822e2fb29be0e401d4cf1c5b0d104
farunurisonmez <info@farunurisonmez.com>
2023-05-31 09:39:35 +0300

    "Render accordion list items based on expeditions data from expeditions.json"

--- src/components/navigation/drawer/geo.map.drawer.tsx
+++ src/components/navigation/drawer/geo.map.drawer.tsx
@@ -1,13 +1,15 @@
 import React from "react"
-import {Card, CardContent, List} from "@mui/material";
-
+import {Accordion, AccordionSummary, Card, CardContent, List, ListItem} from "@mui/material";
+import expeditionsData from "../../../data/expeditions/expeditions.json"
 
 const GeoMapDrawer = () => {
+
+
     return(
         <Card sx={{
             backgroundColor: "smoke",
             position: "absolute",
-            height: "100vh",
+            height: "99vh",
             width: 400,
             zIndex: 500,
             background: "rgba(35,35,35,.8)"
@@ -20,10 +22,24 @@ const GeoMapDrawer = () => {
                         backgroundColor:"rgba(0,0,0,0.7)",
                         position: 'relative',
                         overflow: 'auto',
-                        maxHeight: '800px',
+                        maxHeight: '700px',
                     }}
                 >
-
+                    {
+                        expeditionsData.map((item:any)=>(
+                            <ListItem>
+                                <Accordion
+                                    sx={{
+                                        width:"100%"
+                                    }}
+                                >
+                                    <AccordionSummary>
+                                        {item.expedition_no}
+                                    </AccordionSummary>
+                                </Accordion>
+                            </ListItem>
+                        ))
+                    }
                 </List>
             </CardContent>
         </Card>
