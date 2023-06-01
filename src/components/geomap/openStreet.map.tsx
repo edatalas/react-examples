80d6a32bda429d61c325485852320c3c4f48afef
farunurisonmez <info@farunurisonmez.com>
2023-06-02 01:22:40 +0300

    The code snippet defines several state variables using the useState hook. These variables are circleCenter, circleRadius, isDragging, prevMousePosition, isSecondClick, and isDrawing.
    The handleMapClick function is triggered when a click event occurs on the map. It checks the state variables to determine the click's purpose. If circleCenter is null, it sets the clicked coordinates as the center of the circle. If isSecondClick is false, it sets isSecondClick to true. If both conditions are met, it calculates the distance between the center and the clicked coordinates and sets it as the circle's radius. It also updates the state variables accordingly.
    
    The calculateDistance function calculates the distance between two sets of latitude and longitude coordinates using the Haversine formula.
    
    The deg2rad function converts degrees to radians.
    
    The CircleLabel component is a popup label that displays the circle's radius. It uses the useMap hook to access the Leaflet map instance and the useEffect hook to update the label's position when the circleCenter changes.
    
    The handleMouseMove function is triggered when a mouse move event occurs on the map. It checks if dragging is enabled and if circleCenter is set. If dragging is enabled and isSecondClick is false, it calculates the distance between the current mouse position and the circle's center, as well as the distance from the previous mouse position to the center. It then calculates the difference between these distances and updates the circle's radius accordingly.
    
    The handleMouseUp function is triggered when a mouse up event occurs on the map. It disables drawing by setting isDrawing to false.
    
    The handleMouseDown function is triggered when a mouse down event occurs on the map. It enables dragging by setting isDragging to true.
    
    The MapEventsHandler component sets up event listeners for the map. It uses the useMap hook to access the Leaflet map instance and registers the event listeners for click, mousemove, mousedown, and mouseup events. It also cleans up the event listeners when the component is unmounted.
    
    The lineCoordinates variable is an array of coordinates used to draw a line representing the radius of the circle on the map. It is updated based on the circleCenter and circleRadius values.

--- src/components/geomap/openStreet.map.tsx
+++ src/components/geomap/openStreet.map.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React,{useState, useRef, useEffect } from 'react'
 
-import { MapContainer, Marker, Popup, TileLayer, useMap, ZoomControl} from "react-leaflet";
+import { MapContainer,
+    Marker,
+    Popup,
+    TileLayer,
+    useMap,
+    ZoomControl,
+    useMapEvents,
+    Circle,
+    Polyline,
+    Tooltip
+} from "react-leaflet";
+import { LatLngExpression } from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 import {Grid} from "@mui/material";
@@ -18,7 +29,121 @@ const OpenStreeMap = ({drawers, leftDrawerOpen}:IOpenStreetMap) => {
         iconSize: [32, 32],
     });
 
+    const [circleCenter, setCircleCenter] = useState<[number, number] | null>(null);
+    const [circleRadius, setCircleRadius] = useState<number>(0);
+
+    const [isDragging, setIsDragging] = useState<boolean>(false);
+    const prevMousePosition = useRef<[number, number] | null>(null);
+
     const DescriptionWithMap = withMap(MapDrawerButton);
+    const [isSecondClick, setIsSecondClick] = useState(false);
+
+    const [isDrawing, setIsDrawing] = useState(false);
+
+
+    const handleMapClick = (event: L.LeafletMouseEvent) => {
+        const { lat, lng } = event.latlng;
+        if (!circleCenter) {
+            setCircleCenter([lat, lng]);
+            setIsSecondClick(false);
+        } else if (!isSecondClick) {
+            setIsSecondClick(true);
+        } else {
+            if (!circleRadius) {
+                const distance = calculateDistance(circleCenter[0], circleCenter[1], lat, lng);
+                setCircleRadius(distance);
+            }
+            setIsSecondClick(false);
+            setIsDrawing(false); // İşlem tamamlandığında çizim işaretleyicisini devre dışı bırak
+
+        }
+    };
+
+    const calculateDistance = (lat1: number, lon1: number, lat2: number, lon2: number) => {
+        const R = 6371; // Yeryüzü'nün ortalama yarıçapı (km)
+        const dLat = deg2rad(lat2 - lat1);
+        const dLon = deg2rad(lon2 - lon1);
+        const a =
+            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        const distance = R * c;
+        return distance;
+    };
+
+    const deg2rad = (deg: number) => {
+        return deg * (Math.PI / 180);
+    };
+
+    const CircleLabel = () => {
+        const map = useMap();
+        const labelRef = useRef<HTMLDivElement | null>(null);
+
+        useEffect(() => {
+            if (circleCenter && labelRef.current) {
+                const labelPosition = map.latLngToContainerPoint(circleCenter);
+                labelRef.current.style.position = 'absolute';
+                labelRef.current.style.transform = `translate(-50%, -50%)`;
+                labelRef.current.style.left = `${labelPosition.x}px`;
+                labelRef.current.style.top = `${labelPosition.y}px`;
+            }
+        }, [circleCenter, map]);
+
+        return (
+
+            <Popup position={lineCoordinates[1]}>{circleRadius.toFixed(2)} km</Popup>
+        )
+    };
+
+    const handleMouseMove = (event: L.LeafletMouseEvent) => {
+        if (isDragging && circleCenter && !isSecondClick) {
+            const { lat, lng } = event.latlng;
+            const prevLat = prevMousePosition.current ? prevMousePosition.current[0] : lat;
+            const prevLng = prevMousePosition.current ? prevMousePosition.current[1] : lng;
+            const distance = Math.sqrt(Math.pow(lat - circleCenter[0], 2) + Math.pow(lng - circleCenter[1], 2));
+            const prevDistance = Math.sqrt(Math.pow(prevLat - circleCenter[0], 2) + Math.pow(prevLng - circleCenter[1], 2));
+            const radiusDelta = (distance - prevDistance) * 100000;
+            setCircleRadius((prevRadius) => Math.max(prevRadius + radiusDelta, 0)); // Negatif yarıçapı engelle
+            prevMousePosition.current = [lat, lng];
+        }
+    };
+
+    const handleMouseUp = () => {
+        setIsDrawing(false);
+
+    };
+
+    const handleMouseDown = () => {
+        setIsDragging(true);
+    };
+
+
+    const MapEventsHandler = () => {
+        const map = useMap();
+        map.on('click', handleMapClick)
+        map.on('mousemove', handleMouseMove);
+        map.on('mousedown', handleMouseDown);
+        map.on('mouseup', handleMouseUp);
+        useEffect(() => {
+            if (isDrawing) {
+
+                map.on('mousemove', handleMouseMove);
+                map.on('mousedown', handleMouseDown);
+                map.on('mouseup', handleMouseUp);
+            }
+            return () => {
+                map.off('mousemove', handleMouseMove);
+                map.off('mousedown', handleMouseDown);
+                map.off('mouseup', handleMouseUp);
+                map.off('click', handleMapClick)
+            };
+        }, [map, isDrawing]);
+
+        return null;
+    };
+
+
+    const lineCoordinates: LatLngExpression[] = circleCenter ? [[...circleCenter], [circleCenter[0] + circleRadius / 100000, circleCenter[1]]] : [];
 
     return(
         <Grid>
@@ -33,7 +158,9 @@ const OpenStreeMap = ({drawers, leftDrawerOpen}:IOpenStreetMap) => {
                     height: '99vh',
                     position:"absolute",
                     overflow:""
-                }}>
+                }}
+            >
+
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
@@ -42,8 +169,25 @@ const OpenStreeMap = ({drawers, leftDrawerOpen}:IOpenStreetMap) => {
                         Truck Location
                     </Popup>
                 </Marker>
+                {circleCenter && <Circle center={circleCenter} radius={circleRadius} />}
+                {lineCoordinates.length > 0 && (
+                    <>
+                        <Polyline positions={lineCoordinates}/>
+                    </>
+                )}
+
+                {circleCenter && circleRadius && (
+                    <>
+                        <Circle center={circleCenter} radius={circleRadius} />
+                        <CircleLabel/>
+                    </>
+                )}
+                {circleCenter && <CircleLabel />}
+                {circleCenter && <MapEventsHandler />}
+
                 <DescriptionWithMap drawer={(status)=>leftDrawerOpen(status)}></DescriptionWithMap>
                 <ZoomControl position={"bottomright"}/>
+                <MapEventsHandler />
             </MapContainer>
         </Grid>
     )
